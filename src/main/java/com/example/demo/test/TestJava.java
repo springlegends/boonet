ea06a7e757f30d57cde75afea4608c0d9ba0fec3
wanm <1097736928@qq.com>
2019-02-28 17:04:34 +0800

    万铭第二次修改

--- src/main/java/com/example/demo/test/TestJava.java
+++ src/main/java/com/example/demo/test/TestJava.java
@@ -7,6 +7,6 @@ package com.example.demo.test;
 public class TestJava {
 
     public static void main(String[] args) {
-        System.out.println("万铭修改代码");
+        System.out.println("万铭第二次修改代码");
     }
 }
